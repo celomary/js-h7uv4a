6d73af3695b42b5ae242ec8155240da8886d2362
celomary <59455493+celomary@users.noreply.github.com>
2022-11-16 17:26:21 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,76 +1,29 @@
-import canvasSketch from 'canvas-sketch';
-import { math, random, color } from 'canvas-sketch-util';
-import colors from 'riso-colors';
+import canvaSketch from 'canvas-sketch';
 
 const settings = {
   dimensions: [1080, 1080],
 };
 
-const getX = (degree, radius) => Math.cos(math.degToRad(degree)) * radius;
-const getY = (degree, radius) => Math.sin(math.degToRad(degree)) * radius;
-
-const drawSkewedRect = (
-  context,
-  x,
-  y,
-  w,
-  h,
-  angle = 30,
-  fill = 'blue',
-  stroke = 'black'
-) => {
-  let xi, yi;
-  xi = getX(angle, w);
-  yi = getY(angle, w);
-  context.save();
-  context.strokeStyle = stroke;
-  context.fillStyle = fill;
-  context.lineWidth = 10;
-  const shadowColor = color.offsetHSL(fill, 0, 0, -20).rgba;
-  shadowColor[3] = .5
-  context.shadowColor = color.style(shadowColor)
-  context.shadowOffsetX = 5;
-  context.shadowOffsetY = 5;
-  context.translate(x - xi * 0.5, y - yi * 0.5);
-  context.beginPath();
-  context.moveTo(0, 0);
-  context.lineTo(xi, yi);
-  context.lineTo(xi, yi + h);
-  context.lineTo(0, h);
-  context.closePath();
-  context.stroke();
-  context.fill();
-  context.restore();
-};
-const sketch = ({ width, height }) => {
-  let x, y, w, h;
-  const rect = [];
-  const colorRect = [
-    random.pick(colors).hex,
-    random.pick(colors).hex,
-    random.pick(colors).hex,
-  ];
-
-  console.log(random.pick(colors));
-  for (let num = 0; num < 20; num++) {
-    rect.push({
-      x: random.range(0, width),
-      y: random.range(0, height),
-      w: width * 0.5,
-      h: height * 0.1,
-      fill: random.pick(colors).hex,
-      stroke: random.pick(colors).hex,
-      angle: -30,
-    });
-  }
-
+const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = '#f0f0f0';
     context.fillRect(0, 0, width, height);
-    rect.forEach(({ x, y, w, h, angle, fill, stroke }) => {
-      drawSkewedRect(context, x, y, w, h, angle, fill, stroke);
-    });
+    const centerX = width / 2;
+    const centerY = height / 2;
+    context.save();
+    context.translate(centerX, centerY);
+    const rectWidth = 300;
+    const rectHeight = 50;
+    context.fillStyle = '#000';
+    context.lineWidth = 10;
+    context.beginPath();
+    context.moveTo(-rectWidth / 2, -rectHeight / 2);
+    context.lineTo(rectWidth, -rectHeight / 2);
+    context.lineTo(rectWidth, rectHeight);
+    context.lineTo(-rectWidth / 2, rectHeight);
+    context.closePath();
+    context.stroke();
   };
 };
 
-canvasSketch(sketch, settings);
+canvaSketch(sketch, settings);
